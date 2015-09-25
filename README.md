e5c7baac3d02f771bca3c4b5b11fb4af4a3ee8d4
Suresh Cherukuri <cherukuri7@yahoo.com>
2015-09-25 15:26:05 -0500

    Modified README.md and R Script

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 getdata-032 Course Project on Tidy Data
 =========
-This repository contains step-by-step instructions to process raw data from [Samsung smart phones](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip). For more information on the data set, please visit the [link](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) 
+This repository (repo) contains step-by-step instructions to process experimental data from [Samsung smart phones](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip). For more information on the data set, please visit the [link](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).
+
+The repo contains the following two files:
+
+1. run_analysis.R
+2. CodeBook.md
+
+## Understanding run_analysis.R
+The R script contains all the required code to process the raw data files and output a tidy data set with average of each variable for each activity and each subject. IMPORTANT: It is assumed that required raw data is already unzipped into the working directory of R. The script has four main sections
+
+1. Read all raw files into R
+2. Combine both 'test' and 'train' data sets into one master data set and cleanup variable names
+3. Subset the master data set so that only variables pertaining to "mean" or "std" are selected for further processing.
+4. Group and summarize variables by averaging each variable for each activity and subject. A tidy data set called 'tidy-data.txt' is written to working directory and uploaded for peer evaluation.
+
+## Understanding CodeBook.md
+The code book explains each variable, their position, and values or explantion.
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-# Read all data files into R
+# Part 1: Read all data files into R
 subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE,sep ="" )
 y_test<-read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE,sep ="" )
 x_test<-read.table("./UCI HAR Dataset/test/x_test.txt",header=FALSE,sep ="" )
@@ -8,7 +8,7 @@ x_train<-read.table("./UCI HAR Dataset/train/x_train.txt",header=FALSE,sep ="" )
 features<-read.table("./UCI HAR Dataset/features.txt",header=FALSE,sep ="" )
 activity_labels<-read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE,sep ="" )
 
-# Create a master data frame from both data sets (test and train)
+# Part 2: Create a master data frame from both data sets (test and train)
 #Create a new column called "Treatment" so that analysis can be done on test and train factors
 # Set col names including activity labels and subject IDs
 # used make.names() to clean up features' names and to comply with R naming rules
@@ -18,7 +18,7 @@ train_data<-cbind(Treatment="Train",subject_train,y_train,x_train)
 master_data<-rbind(test_data,train_data)
 colnames(master_data)[1:564]<-c("Treatment","Subject_ID","Activity",make.names(features$V2,unique = TRUE))
 
-# Create data_set by subsetting only means and std dev columns from master_data for further analysis
+# Part 3: Create data_set by subsetting only means and std dev columns from master_data for further analysis
 library(dplyr)
 data_set<-select(master_data,Treatment,Subject_ID,Activity,contains("mean",ignore.case=TRUE),contains("std",ignore.case=TRUE))
 
@@ -27,7 +27,7 @@ data_set<-merge(data_set,activity_labels,by.x = "Activity",by.y = "V1",all = TRU
 data_set<-select(data_set,Treatment,Subj_ID=Subject_ID,Activity=V2,tBodyAcc.mean...X:fBodyBodyGyroJerkMag.std..)
 
 
-# Group, summarize and arrange to create tidy_data
+# Part 4: Group, summarize and arrange to create a tidy_data file
 tidy_data<-group_by(data_set,Treatment,Activity,Subj_ID)
 tidy_data<-summarise_each(tidy_data,c("mean"))
 tidy_data<-arrange(tidy_data,Treatment,Subj_ID,Activity)
