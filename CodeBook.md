ec0296906258dd31c6d4ad16d2742a5adc2fe815
Suresh Cherukuri <cherukuri7@yahoo.com>
2015-09-25 16:44:29 -0500

    started working on CodeBook.md

---
+++ CodeBook.md
@@ -0,0 +1,7 @@
+# Data Dictionary - tidy-data.txt
+
+|Variable          | Position |Values or Explantion  |
+| :-------------   |:---------| :-----|
+| treatment        | 1       |factor of two levels (TEST, TRAIN). Allows to analyze on test data set versus train data set |
+|activity          | 2       |factor of six levels (WALKING, WALKING_UPSTAIRS,WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) |
+| subjectid        | 3      |factor of 30 levels representing 30 people who participated in the experiment. They belong to TEST and TRAIN treatments |
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-# Part 1: Read all data files into R
+# Part1: Read all data files into R
 subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE,sep ="" )
 y_test<-read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE,sep ="" )
 x_test<-read.table("./UCI HAR Dataset/test/x_test.txt",header=FALSE,sep ="" )
@@ -11,26 +11,27 @@ activity_labels<-read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE
 # Part 2: Create a master data frame from both data sets (test and train)
 #Create a new column called "Treatment" so that analysis can be done on test and train factors
 # Set col names including activity labels and subject IDs
-# used make.names() to clean up features' names and to comply with R naming rules
+# used gsub() and make.names() to clean up features' names and to comply with R naming rules
 
 test_data<-cbind(Treatment="Test",subject_test,y_test,x_test)
 train_data<-cbind(Treatment="Train",subject_train,y_train,x_train)
 master_data<-rbind(test_data,train_data)
-colnames(master_data)[1:564]<-c("Treatment","Subject_ID","Activity",make.names(features$V2,unique = TRUE))
+colnames(master_data)[1:564]<-c("treatment","subjectid","activity",make.names(features$V2,unique = TRUE))
+colnames(master_data)<-gsub("\\.","",colnames(master_data))
 
-# Part 3: Create data_set by subsetting only means and std dev columns from master_data for further analysis
+# Part3: Create data_set by subsetting only means and std dev columns from master_data for further analysis
 library(dplyr)
-data_set<-select(master_data,Treatment,Subject_ID,Activity,contains("mean",ignore.case=TRUE),contains("std",ignore.case=TRUE))
+data_set<-select(master_data,treatment,subjectid,activity,contains("mean",ignore.case=TRUE),contains("std",ignore.case=TRUE))
 
 # Merge data with activity_labels to replace ID with activity descriptions
-data_set<-merge(data_set,activity_labels,by.x = "Activity",by.y = "V1",all = TRUE)
-data_set<-select(data_set,Treatment,Subj_ID=Subject_ID,Activity=V2,tBodyAcc.mean...X:fBodyBodyGyroJerkMag.std..)
+data_set<-merge(data_set,activity_labels,by.x = "activity",by.y = "V1",all = TRUE)
+data_set<-select(data_set,treatment,subjectid,activity=V2,tBodyAccmeanX:fBodyBodyGyroJerkMagstd)
 
 
-# Part 4: Group, summarize and arrange to create a tidy_data file
-tidy_data<-group_by(data_set,Treatment,Activity,Subj_ID)
+# Part 4: Group, summarize and arrange to create tidy_data
+tidy_data<-group_by(data_set,treatment,activity,subjectid)
 tidy_data<-summarise_each(tidy_data,c("mean"))
-tidy_data<-arrange(tidy_data,Treatment,Subj_ID,Activity)
+tidy_data<-arrange(tidy_data,treatment,subjectid,activity)
 
 # Output file to text file
 write.table(tidy_data,file="tidy_data.txt",sep="|",row.names = FALSE)
